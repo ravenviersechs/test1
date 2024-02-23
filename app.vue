7b8c3fcef5e306b53562b342c152431fbe402372
ravenviersechs <iphone@okuehnel.de>
2024-02-23 10:10:59 +0100

    Hinzufügen von Links zu den Seiten "Über uns" und "Kontakt" in app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
     <NuxtWelcome />
+    <div>
+      <NuxtLink to="/about">About page</NuxtLink>
+      <NuxtLink to="/contact">Contact page</NuxtLink>
+    </div>
   </div>
 </template>
